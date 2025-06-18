c240c55f067abbc0b97a2479b27b9611983f1597
Pushp Hans <pushp.hans1502@gmail.com>
2025-06-18 13:46:02 +0530

    Commit

--- app/src/main/java/com/example/portfolio/fragments/ContactFragment.kt
+++ app/src/main/java/com/example/portfolio/fragments/ContactFragment.kt
@@ -31,6 +31,9 @@ class ContactFragment : Fragment(R.layout.fragment_contact) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
+            findNavController().popBackStack(R.id.homeFragment, false)
+        }
 
 
         binding.cvNumber.setOnClickListener {
